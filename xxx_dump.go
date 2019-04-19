db10307854d345242502e1279984ffc844352b3b
Takayuki Ujiie <Takayuki.Ujiie@sony.com>
2019-04-19 13:59:00 +0900

    .

--- xxx_dump.go
+++ xxx_dump.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-  // "fmt"
-  // "os"
+  "fmt"
+  "io"
+  "os"
+  "bufio"
 )
 
+func isTokenSound(token XXXToken) bool {
+  return true
+}
+
+func DumpVar(token XXXToken) XXXData {
+  return nil
+}
+
+func DumpToken(token XXXToken, env *XXXEnv) XXXData {
+  if !isTokenSound(token) {
+    fmt.Fprintf(os.Stderr, "ERROR: lines have to be composed by bytes")
+    os.Exit(1)
+  }
+
+  return nil
+}
+
 func Dump(dst string, pool []XXXToken, env *XXXEnv) {
-  // file, err := os.Open(dst)
-  // if err != nil {
-  //   panic(err)
-  // }
-  //
-  // fmt.Println(file)
+  fmt.Println(pool)
+
+  var file io.Writer
+  if dst == "" {
+    file = os.Stdout
+  } else {
+    file, err := os.Create(dst)
+    if err != nil {
+      panic(err)
+    }
+    defer file.Close()
+  }
+
+  writer := bufio.NewWriter(file)
+  for _, token := range pool {
+    data := DumpToken(token, env)
+    _, err := writer.Write(data)
+    if err != nil {
+      panic(err)
+    }
+  }
 }
--- xxx_parse.go
+++ xxx_parse.go
@@ -198,6 +198,7 @@ func Parse(src string) ([]XXXToken, *XXXEnv) {
   if err != nil {
     panic(err)
   }
+  defer file.Close()
 
   scanner := bufio.NewScanner(file)
   for scanner.Scan() {
